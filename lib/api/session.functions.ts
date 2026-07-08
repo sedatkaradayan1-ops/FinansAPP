3b6dc4042056ea1bde4bca7f2050e53a5ae9a0f4
sedatkaradayan1-ops <sedatkaradayan1@gmail.com>
2026-07-08 13:33:04 +0300

    Delete lib directory

--- lib/api/session.functions.ts
+++
@@ -1,8 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-
-import { readCurrentUser, type CurrentUser } from "../auth.server";
-
-export const getSessionUser = createServerFn({ method: "GET" }).handler(
-  async (): Promise<CurrentUser> => readCurrentUser(),
-);
-
